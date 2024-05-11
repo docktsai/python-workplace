2f59351360ae5857bd914d78f64cad2316caed35
Dock Tsai <docktsai@gmail.com>
2024-05-10 22:33:29 -0400

    1st  comit

---
+++ test1.py
@@ -0,0 +1,11 @@
+""" this is just a test modul to test some code """
+import datetime
+from dataclasses import dataclass
+
+@dataclass
+class ApaInfo:
+    api_id: str
+    create_date: datetime
+    update_date: datetime
+
+x = 
